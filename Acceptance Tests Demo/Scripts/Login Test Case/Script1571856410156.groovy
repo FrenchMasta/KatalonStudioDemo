ccd658857c395d2dc3e66e823e9d2a07780b23cc
Jp <jean-pierre.roussel@absa.africa>
2019-10-23 21:12:22 +0200

    Reliably close browser regardless of test outcome - using test variable
    Also formatted code using Ctrl + Shift + F

--- Acceptance Tests Demo/Scripts/Login Test Case/Script1571856410156.groovy
+++ Acceptance Tests Demo/Scripts/Login Test Case/Script1571856410156.groovy
@@ -1,27 +1,40 @@
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-
+import com.kms.katalon.core.exception.StepFailedException as StepFailedException
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
 
-import internal.GlobalVariable
+try {
+    WebUI.openBrowser('')
 
-WebUI.openBrowser('')
+    WebUI.navigateToUrl(GlobalVariable.BaseURL)
 
-WebUI.navigateToUrl(GlobalVariable.BaseURL)
+    WebUI.waitForPageLoad(3)
 
-WebUI.waitForPageLoad(3)
+    WebUI.verifyElementClickable(findTestObject('Object Repository/Login OR/BurgerIcon Side Menu'))
 
-WebUI.verifyElementClickable(findTestObject('Object Repository/Login OR/BurgerIcon Side Menu'))
+    //WebUI.click(findTestObject('Object Repository/Login OR/BurgerIcon Side Menu'))
 
-WebUI.click(findTestObject('Object Repository/Login OR/BurgerIcon Side Menu'))
+    WebUI.verifyElementClickable(findTestObject('Object Repository/Login OR/Login Option (Side Menu)'))
 
-WebUI.verifyElementClickable(findTestObject('Object Repository/Login OR/Login Option (Side Menu)'))
+    WebUI.click(findTestObject('Object Repository/Login OR/Login Option (Side Menu)'))
 
-WebUI.click(findTestObject('Object Repository/Login OR/Login Option (Side Menu)'))
+    WebUI.setText(findTestObject('Object Repository/Login OR/Input Username'), GlobalVariable.Username)
 
-WebUI.setText(findTestObject('Object Repository/Login OR/Input Username'), GlobalVariable.Username)
+    WebUI.setEncryptedText(findTestObject('Object Repository/Login OR/Input Password'), GlobalVariable.EncryptedPasword)
 
-WebUI.setEncryptedText(findTestObject('Object Repository/Login OR/Input Password'), GlobalVariable.EncryptedPasword)
+    WebUI.verifyElementClickable(findTestObject('Object Repository/Login OR/Button Login'))
 
-WebUI.verifyElementClickable(findTestObject('Object Repository/Login OR/Button Login'))
-
-WebUI.click(findTestObject('Object Repository/Login OR/Button Login'))
+    WebUI.click(findTestObject('Object Repository/Login OR/Button Login'))
+}
+catch (StepFailedException sfe) {
+    throw sfe
+} 
+catch (Exception e) {
+    throw e
+} 
+finally { 
+	// Regardless of test outcome/state determine if browser should be closed
+    if (shouldCloseBrowser) {
+        WebUI.closeBrowser()
+    }
+}
